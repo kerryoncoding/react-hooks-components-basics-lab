caab4c9c59e310cbf486d54d6eb3c6c63239b7cd
Kerry Guarino <kerry.guarino@gmail.com>
2023-06-21 20:52:26 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,39 @@
 import React from "react";
+import NavBar from "./Navbar";
+import Home from "./Home";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
+// function NavBar() {
+//   return (
+//     <nav>
+//       <a href="#home">I'm a link!</a>
+//     </nav>
+//   );
+// }
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
+// function Home() {
+//   return (
+//     <div id="home">
+//       <h1>Home</h1>
+//     </div>
+//   );
+// }
+
+function About(){
+  return(
+    <div>
+      <h2 id="about">About</h2>
     </div>
-  );
+  )
 }
 
-{/* write an <About> component here */}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
